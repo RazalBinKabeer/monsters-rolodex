240b0846caf6628e28416c08b250b510f6e63b97
Razal <razalbinkabeer01@gmail.com>
2023-05-26 18:42:24 +0400

    key added in CardList component

--- src/components/CardList/CardList.component.jsx
+++ src/components/CardList/CardList.component.jsx
@@ -2,7 +2,7 @@ import "./CardList.styles.css";
 import Card from "../Card/Card.component";
 
 const CardList = ({ list }) => (
-  <div className="card-list">
+  <div className="card-list" key={list.id}>
     {list.map((monster) => {
       return <Card cardInfo={monster} />;
     })}
